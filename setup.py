9048e406bd3939959ac4a5497241d17ab025ed9f
Josh Campbell <joshcb@amazon.com>
2016-03-26 11:28:34 -0500

    adds module setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lambda_sample_events',
+      version='0.1.0',
+      description='Python module for creating sample events to test AWS Lambda functions.',
+      url='https://github.com/irlrobot/python_lambda_sample_events',
+      author='Josh Campbell',
+      author_email='josh@userdel.com',
+      license='Apache 2.0',
+      packages=['lambda_sample_events'],
+      include_package_data=True,
+      zip_safe=False)

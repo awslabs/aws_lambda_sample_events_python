3b95806b3f8c36596390266d0ba59dff9d6d587a
Josh Campbell <joshcb@amazon.com>
2016-03-30 10:44:47 -0500

    updates packaged name to aws_lambda_sample_events

--- lambda_sample_events/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Python module for creating sample events to test AWS Lambda functions.
-"""
-from lambda_sample_events.sample_event import SampleEvent
--- lambda_sample_events/unknown_service_error.py
+++
@@ -1,6 +0,0 @@
-"""
-UnknownServiceError exception for when an invalid service name is used
-"""
-class UnknownServiceError(Exception):
-    """ UnknownServiceError exception """
-    pass

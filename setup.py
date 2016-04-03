59f3a472a7b085dc3c92d467bf77a205f5c37e65
Josh Campbell <joshcb@amazon.com>
2016-04-03 12:10:34 -0500

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aws_lambda_sample_events',
-      version='1.0.0',
+      version='1.0.1',
       description='A Python module for creating sample events to test AWS Lambda functions.',
       long_description='See README.md in source.',
       url='https://github.com/awslabs/aws_lambda_sample_events_python',
